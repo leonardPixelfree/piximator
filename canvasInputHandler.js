b759b96c017c7cb20c0f05e2a9fd8a754b81da0f
leonard <leonard@pixelfreestudio.com>
2023-07-11 20:28:28 +0200

    prepare direct canvas input

---
+++ canvasInputHandler.js
@@ -0,0 +1,14 @@
+
+function canvasClicked(event){
+    if(event.button === 0){
+
+    } else if (event.button === 2){
+        
+    }
+
+    console.log(event);
+}
+
+function canvasScrolled(event){
+    console.log(event);
+}
